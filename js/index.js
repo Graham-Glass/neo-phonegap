c232b7bdd2112606754a6adffc042340ad73e7b7
Sergiu Nichitean <sergiu.nichitean@artsoft-consult.ro>
2015-02-26 16:04:59 +0200

    android push notification update

--- js/index.js
+++ js/index.js
@@ -880,6 +880,8 @@ var app = {
     	switch(e.event){ 
     		case 'registered': 
     			if (e.regid.length > 0){ 
+    				console.log('Reg ID: ' + e.regid); 
+    				console.log('Message: ' + e.msg); 
     				app.storeToken(e.regid);
     			} 
     			break;   
