fb7535121c468c07d24fe3f509d6c0b2e533d04a
sergiu-nichitean <sergiu.nichitean@artsoft-consult.ro>
2015-10-02 15:59:42 +0300

    new jwplayer

--- js/index.js
+++ js/index.js
@@ -1027,7 +1027,7 @@ var app = {
                 console.log('loginCredentials found, opening ' + currentSchool.replace('?mobile_app=true', 'log_in/submit?userid=' + loginCredentialsArray[0] + '&password=' + loginCredentialsArray[1] + '&mobile_app=true'));
                 $('#contentFrame').attr('src', currentSchool.replace('?mobile_app=true', 'log_in/submit?userid=' + loginCredentialsArray[0] + '&password=' + loginCredentialsArray[1] + '&mobile_app=true&new_jwplayer=true'));
             } else {
-                $('#contentFrame').attr('src', currentSchool);
+                $('#contentFrame').attr('src', currentSchool + '&new_jwplayer=true');
             }
             app.toggleLoader(false);
         };
